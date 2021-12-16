304bcb3dab1ce6818251504c54ea6f0034758d46
CheStix <che.eldar@gmail.com>
2021-12-16 13:20:02 +0300

    2 Dealing with Strings

--- Chapter_2/main.go
+++ Chapter_2/main.go
@@ -1,6 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"unicode/utf8"
+)
+
+const publisher = "Wiley"
 
 var (
 	firstName string = "Wei-Meng"
@@ -16,6 +21,15 @@ func main() {
 	var raining bool        // boolean variable
 	var rates float32 = 4.5 //declared as float and then initialized
 
+	address := "The White House\n1600 Pennsylvania Avenue NW\nWashington, DC 20500"
+	quotation := `"Any who never made
+a mistake has never tried
+anything new"
+--Albert Einstein`
+	str1 := "你好,世界"
+	str2 := "こんにちは世界"
+	str3 := "\u4f60\u597d\uff0c\u4e16\u754c"
+
 	fmt.Println(num1)    // 5
 	fmt.Println(num2)    // 0
 	fmt.Println(num3)    // 0
@@ -23,4 +37,10 @@ func main() {
 	fmt.Println(raining) // false
 	fmt.Println(rates)   // 4.5
 	fmt.Println(firstName, lastName, age)
+	fmt.Println(publisher)
+	fmt.Println(address)
+	fmt.Println(quotation)
+	fmt.Println(str1, len(str1), utf8.RuneCountInString(str1))
+	fmt.Println(str1, len(str2), utf8.RuneCountInString(str2))
+	fmt.Println(str1, len(str3), utf8.RuneCountInString(str3))
 }
