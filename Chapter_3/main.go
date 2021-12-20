2d45fe3aa73faefefe2e69a853cf85a888b5be04
CheStix <che.eldar@gmail.com>
2021-12-20 17:07:09 +0300

    3 switch statement, fall-through, without condition

--- Chapter_3/main.go
+++ Chapter_3/main.go
@@ -35,4 +35,65 @@ func main() {
 	} else {
 		fmt.Println("handle the error")
 	}
+
+	dayOfWeek := ""
+	switch num {
+	case 1:
+		dayOfWeek = "Monday"
+	case 2:
+		dayOfWeek = "Tuesday"
+	case 3:
+		dayOfWeek = "Wednesday"
+	case 4:
+		dayOfWeek = "Thursday"
+	case 5:
+		dayOfWeek = "Friday"
+	case 6:
+		dayOfWeek = "Saturday"
+	case 7:
+		dayOfWeek = "Sunday"
+	default:
+		dayOfWeek = "--error--"
+	}
+	fmt.Println(dayOfWeek)
+
+	score := 79
+	grade := ""
+	switch {
+	case score < 50:
+		grade = "F"
+	case score < 60:
+		grade = "D"
+	case score < 70:
+		grade = "C"
+	case score < 80:
+		grade = "B"
+	default:
+		grade = "A"
+	}
+
+	switch grade {
+	case "A":
+		fallthrough
+	case "B":
+		fallthrough
+	case "C":
+		fallthrough
+	case "D":
+		fmt.Println("Passed")
+	case "F":
+		fmt.Println("Failed")
+	default:
+		fmt.Println("Absent")
+	}
+
+	switch grade {
+	case "A", "B", "C", "D":
+		fmt.Println("Passed")
+	case "F":
+		fmt.Println("Failed")
+	default:
+		fmt.Println("Undefined")
+	}
+
 }
