2991e66bb43fd0b6c90cbe033876def19b11f78a
CheStix <che.eldar@gmail.com>
2021-12-22 14:01:52 +0300

    4 Iterating through string

--- Chapter_4/main.go
+++ Chapter_4/main.go
@@ -47,4 +47,13 @@ func main() {
 	for i := range OS {
 		fmt.Println(i)
 	}
+
+	for pos, char := range "Hello, World!" {
+		fmt.Println(pos, char)
+		fmt.Printf("%d %c\n", pos, char)
+	}
+
+	for pos, char := range "こんにちは世界" {
+		fmt.Printf("%c at byte location %d\n", char, pos)
+	}
 }
