a65392f58ba80995a86e64215975258ff6f693c8
CheStix <che.eldar@gmail.com>
2021-12-23 10:37:07 +0300

    6 multidimensional arrays

--- Chapter_6/main.go
+++ Chapter_6/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 func main() {
 	var nums1 [5]int // an array of int (5 elements)
@@ -9,4 +12,22 @@ func main() {
 	fmt.Println(nums1)
 	fmt.Println(nums2)
 	fmt.Println(len(nums3))
+
+	var table [5][6]string
+	for row := 0; row < 5; row++ {
+		for column := 0; column < 6; column++ {
+			table[row][column] = strconv.Itoa(row) + "," + strconv.Itoa(column)
+		}
+	}
+	fmt.Println(table)
+
+	var cube [4][3][3]string
+	for row := 0; row < 4; row++ {
+		for column := 0; column < 3; column++ {
+			for depth := 0; depth < 3; depth++ {
+				cube[row][column][depth] = strconv.Itoa(row) + strconv.Itoa(column) + strconv.Itoa(depth)
+			}
+		}
+	}
+	fmt.Println(cube)
 }
