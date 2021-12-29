4c9730e224a8edab513bea0ac0b6e656bd54e2cb
CheStix <che.eldar@gmail.com>
2021-12-29 14:43:22 +0300

    8 using structs and maps, sorting a map of structs

--- Chapter_8/main.go
+++ Chapter_8/main.go
@@ -12,6 +12,19 @@ type kv struct {
 
 type kvPairs []kv
 
+type dob struct {
+	day   int
+	month int
+	year  int
+}
+
+type people struct {
+	name  string
+	email string
+	dob   dob
+}
+
+var members map[int]people
 var heights map[string]int
 
 func (p kvPairs) Len() int {
@@ -82,5 +95,75 @@ func main() {
 	sort.Sort(p)
 	for _, v := range p {
 		fmt.Println(v)
+	}
+
+	members = make(map[int]people)
+	members[1] = people{
+		name:  "Mary Smith",
+		email: "marysmith@mail.com",
+		dob: dob{
+			day:   17,
+			month: 3,
+			year:  1990,
+		},
+	}
+	members[2] = people{
+		name:  "John Smith",
+		email: "johnsmith@mail.com",
+		dob: dob{
+			day:   9,
+			month: 12,
+			year:  1988,
+		},
+	}
+	members[3] = people{
+		name:  "Janet Doe",
+		email: "janetdoe@mail.com",
+		dob: dob{
+			day:   1,
+			month: 12,
+			year:  1988,
+		},
+	}
+	members[4] = people{
+		name:  "Adam Jones",
+		email: "adamjones@mail.com",
+		dob: dob{
+			day:   19,
+			month: 8,
+			year:  2001,
+		},
+	}
+	for k, v := range members {
+		fmt.Println(k, v.name, v.email, v.dob)
+	}
+	// get all keys in members
+	var keys2 []int
+	for k := range members {
+		keys2 = append(keys2, k)
+	}
+	// sort the keys
+	sort.Ints(keys2)
+	// copy the values in members to the slice
+	var sliceMembers []people
+	for _, k := range keys2 {
+		sliceMembers = append(sliceMembers, members[k])
+	}
+	fmt.Println(sliceMembers)
+
+	sort.SliceStable(sliceMembers, func(i, j int) bool {
+		// compare year
+		if sliceMembers[i].dob.year != sliceMembers[j].dob.year {
+			return sliceMembers[i].dob.year < sliceMembers[j].dob.year
+		}
+		// compare month
+		if sliceMembers[i].dob.month != sliceMembers[j].dob.month {
+			return sliceMembers[i].dob.month < sliceMembers[j].dob.month
+		}
+		return sliceMembers[i].dob.day < sliceMembers[j].dob.day
+	})
+
+	for _, v := range sliceMembers {
+		fmt.Println(v.name, v.email, v.dob)
 	}
 }
