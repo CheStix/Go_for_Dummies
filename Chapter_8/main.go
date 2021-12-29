aa8cba406d8d028cb9b2681e53e79cfb6eba4f2a
CheStix <che.eldar@gmail.com>
2021-12-29 14:05:20 +0300

    8 sorting the items in a map by values

--- Chapter_8/main.go
+++ Chapter_8/main.go
@@ -5,7 +5,29 @@ import (
 	"sort"
 )
 
+type kv struct {
+	key   string
+	value int
+}
+
+type kvPairs []kv
+
 var heights map[string]int
+
+func (p kvPairs) Len() int {
+	// returns the length of the collection
+	return len(p)
+}
+
+func (p kvPairs) Less(i, j int) bool {
+	// indicate the first value (height) must be smaller than the second value
+	return p[i].value < p[j].value
+}
+
+func (p kvPairs) Swap(i, j int) {
+	// swap the items in the collection
+	p[i], p[j] = p[j], p[i]
+}
 
 func main() {
 	heights = make(map[string]int)
@@ -48,5 +70,17 @@ func main() {
 	sort.Strings(keys)
 	for _, k := range keys {
 		fmt.Println(k, heights[k])
+	}
+
+	p := make(kvPairs, len(heights))
+	i := 0
+	for k, v := range heights {
+		p[i] = kv{k, v}
+		i++
+	}
+	fmt.Println(p)
+	sort.Sort(p)
+	for _, v := range p {
+		fmt.Println(v)
 	}
 }
