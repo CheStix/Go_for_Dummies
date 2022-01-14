8d429fda76625808b06a74bed446c06aa0e35f0d
CheStix <che.eldar@gmail.com>
2022-01-14 14:23:40 +0300

    encoding interfaces to json

--- Chapter_9/main.go
+++ Chapter_9/main.go
@@ -20,23 +20,27 @@ type Rates struct {
 	Symbol string `json:"destination currency"`
 }
 
-type Name struct {
-	FirstName string
-	LastName  string
-}
+//type Name struct {
+//	FirstName string
+//	LastName  string
+//}
+//
+//type Address struct {
+//	Line1 string
+//	Line2 string
+//	Line3 string
+//}
+//
+//type Customer struct {
+//	Name    Name
+//	Email   string
+//	Address Address
+//	DOB     time.Time
+//}
 
-type Address struct {
-	Line1 string
-	Line2 string
-	Line3 string
-}
-
-type Customer struct {
-	Name    Name
-	Email   string
-	Address Address
-	DOB     time.Time
-}
+type Customer map[string]interface{}
+type Name map[string]interface{}
+type Address map[string]interface{}
 
 func main() {
 	var person People
@@ -97,14 +101,14 @@ func main() {
 	layoutISO := "2006-01-02"
 	dob, _ := time.Parse(layoutISO, "2010-01-18")
 	john := Customer{
-		Name:  Name{FirstName: "John", LastName: "Smith"},
-		Email: "johnsmith@mail.ru",
-		Address: Address{
-			Line1: "The White House",
-			Line2: "1600 Pennsylvania Avenue NW",
-			Line3: "Washington, DC 20500",
+		"Name":  Name{"FirstName": "John", "LastName": "Smith"},
+		"Email": "johnsmith@mail.ru",
+		"Address": Address{
+			"Line1": "The White House",
+			"Line2": "1600 Pennsylvania Avenue NW",
+			"Line3": "Washington, DC 20500",
 		},
-		DOB: dob,
+		"DOB": dob,
 	}
 	johnJSON, err := json.MarshalIndent(john, "", "   ")
 	if err == nil {
