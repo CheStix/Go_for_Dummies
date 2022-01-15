e2d9bd6cadc6dae1fc96cdd6ba5ec87d073c8018
CheStix <che.eldar@gmail.com>
2022-01-15 14:46:12 +0300

    using interfaces

--- Chapter_10/main.go
+++ Chapter_10/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 type DigitsCounter interface {
 	CountOddEven() (int, int)
@@ -20,6 +23,41 @@ func (ds DigitString) CountOddEven() (int, int) {
 	return odds, evens
 }
 
+type Circle struct {
+	radius float64
+	name   string
+}
+type Square struct {
+	length float64
+	name   string
+}
+type Triangle struct {
+	base   float64
+	height float64
+}
+type Shape interface {
+	Area() float64
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * math.Pow(c.radius, 2)
+}
+func (c Circle) Circumference() float64 {
+	return 2 * math.Pi * c.radius
+}
+func (s Square) Area() float64 {
+	return math.Pow(s.length, 2)
+}
+func (t Triangle) Area() float64 {
+	return 0.5 * t.base * t.height
+}
+
+func calculateArea(listOfShapes []Shape) {
+	for _, shape := range listOfShapes {
+		fmt.Println("Area of shape is ", shape.Area())
+	}
+}
+
 func main() {
 	s := DigitString("123456789")
 	fmt.Println(s.CountOddEven())
@@ -27,4 +65,11 @@ func main() {
 	var d DigitsCounter
 	d = s
 	fmt.Println(d.CountOddEven())
+
+	c1 := Circle{radius: 5, name: "c1"}
+	s1 := Square{length: 6, name: "s1"}
+	t1 := Triangle{base: 6, height: 8}
+
+	shapes := []Shape{c1, s1, t1}
+	calculateArea(shapes)
 }
