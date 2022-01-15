8cbaf77e299940e2a1b182f50bbeb5626926c447
CheStix <che.eldar@gmail.com>
2022-01-15 20:35:34 +0300

    access shared resources using mutex

--- Chapter_11/main.go
+++ Chapter_11/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
+	"sync"
 	"time"
 )
 
@@ -11,8 +13,37 @@ func say(s string, times int) {
 		fmt.Println(i, s)
 	}
 }
+
+var balance int
+var mutex = &sync.Mutex{}
+
+func credit() {
+	for i := 0; i < 5; i++ {
+		mutex.Lock()
+		balance += 100
+		time.Sleep(time.Duration(rand.Intn(100)) * time.Millisecond)
+		fmt.Println("After crediting, balance is", balance)
+		mutex.Unlock()
+	}
+}
+
+func debit() {
+	for i := 0; i < 5; i++ {
+		mutex.Lock()
+		balance -= 100
+		time.Sleep(time.Duration(rand.Intn(100)) * time.Millisecond)
+		fmt.Println("After debiting, balance is", balance)
+		mutex.Unlock()
+	}
+}
+
 func main() {
-	go say("Hello", 3)
-	go say("World", 2)
+	//go say("Hello", 3)
+	//go say("World", 2)
+
+	balance = 200
+	fmt.Println("Initial balance is", balance)
+	go credit()
+	go debit()
 	fmt.Scanln()
 }
