14f366551cfa5d636b948d5bf15b9d91054845a8
CheStix <che.eldar@gmail.com>
2022-01-16 12:09:55 +0300

    using channels

--- Chapter_12/main.go
+++ Chapter_12/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"time"
 )
 
@@ -18,9 +19,45 @@ func getData(ch chan string) {
 	time.Sleep(2 * time.Second)
 	fmt.Println("String retrieved from channel:", <-ch)
 }
+
+func sum(s []int, c chan int) {
+	sum := 0
+	for _, v := range s {
+		sum += v
+	}
+	c <- sum
+}
+
 func main() {
 	ch := make(chan string)
 	go sendData(ch)
 	go getData(ch)
+	//fmt.Scanln()
+
+	rand.Seed(time.Now().UnixNano())
+	var s []int
+	sliceSize := 10
+	for i := 0; i < sliceSize; i++ {
+		s = append(s, rand.Intn(100))
+	}
+
+	c := make(chan int)
+	partSize := 2
+	parts := sliceSize / partSize
+	i := 0
+	for i < parts {
+		go sum(s[i*partSize:(i+1)*partSize], c)
+		i += 1
+	}
+
+	i = 0
+	total := 0
+	for i < parts {
+		partialSum := <-c //read from channel
+		fmt.Println("Partial sum:", partialSum)
+		total += partialSum
+		i += 1
+	}
+	fmt.Println("Total:", total)
 	fmt.Scanln()
 }
