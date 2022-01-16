89c89288f15b1473e5675e40d7576b8a39cdbce7
CheStix <che.eldar@gmail.com>
2022-01-16 11:33:41 +0300

    how channels work

---
+++ Chapter_12/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+//send data into channel
+func sendData(ch chan string) {
+	fmt.Println("Sending a string into channel...")
+	//time.Sleep(2 * time.Second)
+	ch <- "Hello"
+	fmt.Println("String has been retrieved from channel..")
+}
+
+//getting data from the channel
+func getData(ch chan string) {
+	time.Sleep(2 * time.Second)
+	fmt.Println("String retrieved from channel:", <-ch)
+}
+func main() {
+	ch := make(chan string)
+	go sendData(ch)
+	go getData(ch)
+	fmt.Scanln()
+}
