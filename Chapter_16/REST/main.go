1e4754dd2509bc4944beabc18f121169705ed2cd
CheStix <che.eldar@gmail.com>
2022-01-21 16:11:58 +0300

    storing the course information on the REST API

--- Chapter_16/REST/main.go
+++ Chapter_16/REST/main.go
@@ -1,18 +1,26 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/gorilla/mux"
+	"io/ioutil"
 	"log"
 	"net/http"
 )
+
+type courseInfo struct {
+	Title string `json:"Title"`
+}
+
+var courses map[string]courseInfo
 
 func home(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Welcome to the REST API!")
 }
 
 func allcourses(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "List of all courses")
+	fmt.Fprintf(w, "List of all courses\n")
 
 	kv := r.URL.Query()
 	for k, v := range kv {
@@ -22,17 +30,97 @@ func allcourses(w http.ResponseWriter, r *http.Request) {
 	if val, ok := kv["country"]; ok {
 		fmt.Println(val[0])
 	}
+
+	// return all the courses in JSON
+	json.NewEncoder(w).Encode(courses)
 }
 
 func course(w http.ResponseWriter, r *http.Request) {
 	params := mux.Vars(r)
-	fmt.Fprintf(w, "Detail for course "+params["courseid"])
+	//fmt.Fprintf(w, "Detail for course "+params["courseid"])
+	//
+	//fmt.Fprintf(w, "\n")
+	//fmt.Fprintf(w, r.Method)
+	if r.Method == "GET" {
+		if _, ok := courses[params["courseid"]]; ok {
+			json.NewEncoder(w).Encode(courses[params["courseid"]])
+		} else {
+			w.WriteHeader(http.StatusNotFound)
+			w.Write([]byte("404 - No course found"))
+		}
+	}
 
-	fmt.Fprintf(w, "\n")
-	fmt.Fprintf(w, r.Method)
+	if r.Method == "DELETE" {
+		if _, ok := courses[params["courseid"]]; ok {
+			delete(courses, params["courseid"])
+			w.WriteHeader(http.StatusNoContent)
+		} else {
+			w.WriteHeader(http.StatusNotFound)
+			w.Write([]byte("404 - No course found"))
+		}
+	}
+
+	if r.Header.Get("Content-type") == "application/json" {
+		// POST is for creating new course
+		if r.Method == "POST" {
+			// read the string sent to the service
+			var newCourse courseInfo
+			reqBody, err := ioutil.ReadAll(r.Body)
+			if err == nil {
+				// convert JSON to object
+				json.Unmarshal(reqBody, &newCourse)
+				if newCourse.Title == "" {
+					w.WriteHeader(http.StatusUnprocessableEntity)
+					w.Write([]byte("422 - Please supply course information in JSON format"))
+					return
+				}
+				// check if course exists; add only if course does not exist
+				if _, ok := courses[params["courseid"]]; !ok {
+					courses[params["courseid"]] = newCourse
+					w.WriteHeader(http.StatusCreated)
+					w.Write([]byte("201 - Course added: " + params["courseid"]))
+				} else {
+					w.WriteHeader(http.StatusConflict)
+					w.Write([]byte("409 - Duplicate course ID"))
+				}
+			} else {
+				w.WriteHeader(http.StatusUnprocessableEntity)
+				w.Write([]byte("422 - Please supply course information in JSON format"))
+			}
+		}
+		// PUT is for creating or updating existing course
+		if r.Method == "PUT" {
+			var newCourse courseInfo
+			reqBody, err := ioutil.ReadAll(r.Body)
+			if err == nil {
+				json.Unmarshal(reqBody, &newCourse)
+				if newCourse.Title == "" {
+					w.WriteHeader(http.StatusUnprocessableEntity)
+					w.Write([]byte("422 - Please supply course information in JSON format"))
+					return
+				}
+				// check if course exists; add only if course does not exist
+				if _, ok := courses[params["courseid"]]; !ok {
+					courses[params["courseid"]] = newCourse
+					w.WriteHeader(http.StatusCreated)
+					w.Write([]byte("201 - Course added: " + params["courseid"]))
+				} else {
+					// update course
+					courses[params["courseid"]] = newCourse
+					w.WriteHeader(http.StatusNoContent)
+				}
+			} else {
+				w.WriteHeader(http.StatusUnprocessableEntity)
+				w.Write([]byte("422 - Please supply course information in JSON format"))
+			}
+		}
+	}
 }
 
 func main() {
+	// instantite courses
+	courses = make(map[string]courseInfo)
+
 	router := mux.NewRouter()
 	router.HandleFunc("/api/v1/", home)
 	router.HandleFunc("/api/v1/courses", allcourses)
