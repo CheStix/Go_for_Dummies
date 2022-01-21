a844129109919a291d892fc32048cd7ddc45edc1
CheStix <che.eldar@gmail.com>
2022-01-21 15:00:33 +0300

    request methods

--- Chapter_16/REST/main.go
+++ Chapter_16/REST/main.go
@@ -27,13 +27,16 @@ func allcourses(w http.ResponseWriter, r *http.Request) {
 func course(w http.ResponseWriter, r *http.Request) {
 	params := mux.Vars(r)
 	fmt.Fprintf(w, "Detail for course "+params["courseid"])
+
+	fmt.Fprintf(w, "\n")
+	fmt.Fprintf(w, r.Method)
 }
 
 func main() {
 	router := mux.NewRouter()
 	router.HandleFunc("/api/v1/", home)
 	router.HandleFunc("/api/v1/courses", allcourses)
-	router.HandleFunc("/api/v1/courses/{courseid}", course)
+	router.HandleFunc("/api/v1/courses/{courseid}", course).Methods("GET", "PUT", "POST", "DELETE")
 
 	fmt.Println("Listening at port 5000")
 	log.Fatal(http.ListenAndServe(":5000", router))
